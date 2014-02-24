bba1ad24a0deeb4ec108d1b480be03c41e46765d
joeMalmsten <malmstenjoey@yahoo.com>
2014-02-24 14:04:59 -0800

    [RALLY-1] added second iteration of the currencyCoverter object. Can now handle all numbers between [-1 * 10^14, 10^14]. Made small changes to adjust for floating point error, also added new test cases to reflect added number magnitudes and removed min value from HTML form

--- trunk/rallyTestScript.js
+++ trunk/rallyTestScript.js
@@ -16,8 +16,12 @@ Members: conversionList - a list containing objects with information on all prev
 function CurrencyContainer() {
 	// Contains all previous function calls(input, output)
 	this.conversionList = [];
-	this.MIN_BOUNDARY = -1000000000000000;
-	this.MAX_BOUNDARY = 1000000000000000;
+	this.MIN_BOUNDARY = -10000000000000;
+	this.MAX_BOUNDARY = 10000000000000;
+	this.QUADRILLION = 1000000000000;
+	this.BILLION = 1000000000;
+	this.MILLION = 1000000;
+	this.THOUSAND = 1000;
 	this.ONES_INDEX_ARRAY = [ 
    		'',
    		'one',
@@ -62,6 +66,7 @@ CurrencyContainer.prototype.convertToCurrency = function(value) {
 	if (typeof(value) !== "number" || value < this.MIN_BOUNDARY || value > this.MAX_BOUNDARY) {
 		return -1;
 	}
+
 	var result = "";
 	if (value < 0) {
 		value *= -1;
@@ -74,18 +79,54 @@ CurrencyContainer.prototype.convertToCurrency = function(value) {
 	// The toFixed call is added to removed any floating point errors we might get
 	var centsValue = Math.floor((value % 1).toFixed(2) * 100);
 	if (centsValue >= 10) {
-		result += " and " + centsValue + "/100";
+		result += "and " + centsValue + "/100";
 	} else {
-		result += " and 0" +  centsValue + "/100";
+		result += "and 0" +  centsValue + "/100";
 	}
 	result += " dollars";
 
 	return result;
 }
 
+CurrencyContainer.prototype.convertQuadrillion = function(value){
+    if (value >= this.QUADRILLION) {
+        return this.convertQuadrillion(Math.floor(value / this.QUADRILLION)) + "quadrillion " + this.convertBillions(value % this.QUADRILLION);
+    }
+    else {
+        return this.convertBillions(value);
+    }
+}
+
+CurrencyContainer.prototype.convertBillions = function(value){
+    if (value >= this.BILLION) {
+        return this.convertBillions(Math.floor(value / this.BILLION)) + "billion " + this.convertMillions(value % this.BILLION);
+    }
+    else {
+        return this.convertMillions(value);
+    }
+}
+
+CurrencyContainer.prototype.convertMillions = function(value){
+    if (value >= this.MILLION) {
+        return this.convertMillions(Math.floor(value / this.MILLION)) + "million " + this.convertThousands(value % this.MILLION);
+    }
+    else {
+        return this.convertThousands(value);
+    }
+}
+
+CurrencyContainer.prototype.convertThousands = function(value){
+    if (value >= this.THOUSAND) {
+        return this.convertThousands(Math.floor(value / this.THOUSAND)) + "thousand " + this.convertHundreds(value % this.THOUSAND);
+    }
+    else{
+        return this.convertHundreds(value);
+    }
+}
+
 CurrencyContainer.prototype.convertHundreds = function(value) {
     if (value >= 100) {
-        return this.ONES_INDEX_ARRAY[Math.floor(value / 100)] + " hundred " + this.convertTens(value % 100);
+        return this.convertTens(Math.floor(value / 100)) + "hundred " + this.convertTens(value % 100);
     } else {
         return this.convertTens(value);
     }
@@ -93,19 +134,27 @@ CurrencyContainer.prototype.convertHundreds = function(value) {
 
 CurrencyContainer.prototype.convertTens = function(value){
     if ( value < 10) {
-		return this.ONES_INDEX_ARRAY[value];
+    	var ret = this.ONES_INDEX_ARRAY[value];
+    	if (value > 0){
+    		ret += " ";
+    	}
+		return ret;
 	} else if (value>=10 && value<20) {
-		return this.TEENS_INDEX_ARRAY[value - 10];
+		return this.TEENS_INDEX_ARRAY[value - 10] + " ";
 	} else {
-        return this.TENS_INDEX_ARRAY[Math.floor(value / 10)] + " " + this.ONES_INDEX_ARRAY[value % 10];
+        var ret = this.TENS_INDEX_ARRAY[Math.floor(value / 10)] + " " + this.ONES_INDEX_ARRAY[value % 10];
+        if ((value % 10) > 0){
+    		ret += " ";
+    	}
+		return ret;
     }
 }
 
 CurrencyContainer.prototype.convertNumber = function(value){
     if (value === 0) {
-    	return "zero";
+    	return "zero ";
     }
-    return this.convertHundreds(value);
+    return this.convertQuadrillion(value);
 }
 
 // This function will call the main conversion function, log the result in our array and return the result object
@@ -235,15 +284,41 @@ myContent.initTestPage();
 //-------------------------- Start unit testing -----------------------------\\
 var assert = require("assert");
 describe ("CurrencyContainer.convertToCurrency():", function() {
-	it ("should return the english string of a given number [-1 * 10^16, 10^16]", function() {
+	it ("should return the english string of a given number [-1 * 10^14, 10^14]", function() {
 		// An array containing possible converstion values and thier converted currency
-		var currencyTestArray = [[0, "zero and 00/100 dollars"], [0.00, "zero and 00/100 dollars"], [0.15, "zero and 15/100 dollars"], [0.154, "zero and 15/100 dollars"], [0.159, "zero and 16/100 dollars"],
-		[1, "one and 00/100 dollars"], [1.01, "one and 01/100 dollars"], [0.01, "zero and 01/100 dollars"], [11.01, "eleven and 01/100 dollars"], [111.01, "one hundred eleven and 01/100 dollars"], 
-		[221.01, "two hundred twenty one and 01/100 dollars"], [911.01, "nine hundred eleven and 01/100 dollars"], [999.99, "nine hundred ninety nine and 99/100 dollars"], 
-		[-0, "zero and 00/100 dollars"], [-0.00, "zero and 00/100 dollars"], [-0.15, "negative zero and 15/100 dollars"], [-0.154, "negative zero and 15/100 dollars"], [-0.159, "negative zero and 16/100 dollars"],
-		[-1, "negative one and 00/100 dollars"], [-1.01, "negative one and 01/100 dollars"], [-0.01, "negative zero and 01/100 dollars"], [-11.01, "negative eleven and 01/100 dollars"], [-111.01, "negative one hundred eleven and 01/100 dollars"], 
-		[-221.01, "negative two hundred twenty one and 01/100 dollars"], [-911.01, "negative nine hundred eleven and 01/100 dollars"], [-999.99, "negative nine hundred ninety nine and 99/100 dollars"]];
+		var currencyTestArray = [[0, "zero and 00/100 dollars"], [0.00, "zero and 00/100 dollars"], [0.15, "zero and 15/100 dollars"], [0.154, "zero and 15/100 dollars"], 
+		[0.159, "zero and 16/100 dollars"], [1, "one and 00/100 dollars"], [1.01, "one and 01/100 dollars"], [0.01, "zero and 01/100 dollars"], [11.01, "eleven and 01/100 dollars"], 
+		[111.01, "one hundred eleven and 01/100 dollars"],  [221.01, "two hundred twenty one and 01/100 dollars"], [911.01, "nine hundred eleven and 01/100 dollars"], 
+		[-0, "zero and 00/100 dollars"], [-0.00, "zero and 00/100 dollars"], [-0.15, "negative zero and 15/100 dollars"], [-0.154, "negative zero and 15/100 dollars"], 
+		[-0.159, "negative zero and 16/100 dollars"], [-1, "negative one and 00/100 dollars"], [-1.01, "negative one and 01/100 dollars"], [-0.01, "negative zero and 01/100 dollars"], 
+		[-11.01, "negative eleven and 01/100 dollars"], [-111.01, "negative one hundred eleven and 01/100 dollars"], [-221.01, "negative two hundred twenty one and 01/100 dollars"], 
+		[-911.01, "negative nine hundred eleven and 01/100 dollars"], 
+		[999.99, "nine hundred ninety nine and 99/100 dollars"], 
+		[9999.99, "nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[99999.99, "ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[999999.99, "nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[9999999.99, "nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[99999999.99, "ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"],
+		[999999999.99, "nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[9999999999.99, "nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"],
+		[99999999999.99, "ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[999999999999.99, "nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"],
+		[9999999999999.99, "nine quadrillion nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"],
+		[10000000000000, "ten quadrillion and 00/100 dollars"],
+		[-999.99, "negative nine hundred ninety nine and 99/100 dollars"],
+		[-9999.99, "negative nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[-99999.99, "negative ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[-999999.99, "negative nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[-9999999.99, "negative nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[-99999999.99, "negative ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"],
+		[-999999999.99, "negative nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[-9999999999.99, "negative nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"],
+		[-99999999999.99, "negative ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"], 
+		[-999999999999.99, "negative nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"],
+		[-9999999999999.99, "negative nine quadrillion nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine and 99/100 dollars"],
+		[-10000000000000, "negative ten quadrillion and 00/100 dollars"]];
 
+		//test our actual value to the value stored in the second index od each test array, the first index is the input for each test
 		var retval = -1;
 		for (var i = 0; i < currencyTestArray.length; ++i) {
 			retval = myContent.currencyContainer.convertToCurrency(currencyTestArray[i][0]);
@@ -251,10 +326,13 @@ describe ("CurrencyContainer.convertToCurrency():", function() {
 		}
 	});
 
-	it ("should return -1 at any index not a non-number or number outside of our bounds [-1 * 10^16, 10^16]", function() {
+	it ("should return -1 at any index not a non-number or number outside of our bounds [-1 * 10^14, 10^14]", function() {
 		// An array containing all invalid inputs
-		var currencyInputArray = ["test", "djflsj;df", "12x2", "0x", "", null, "0", "1", false, true, 10000000000000001, -10000000000000001];
+		var currencyInputArray = ["test", "djflsj;df", "12x2", "0x", "", null, "0", "1", false, true, 
+		                          100000000000001, -100000000000001, "99..99", "99.9qq", 
+		                          ";;;;::::;;;aaajkjkljlnckjsdnjd nhdkshjvlsdahldsa", [0,0,0,0,1,2,2]];
 
+		//make sure we get -1 for all these invalid inputs
 		var retval = -1;
 		for (var i = 0; i < currencyInputArray.length; ++i) {
 			retval = myContent.currencyContainer.convertToCurrency(currencyInputArray[i]);
